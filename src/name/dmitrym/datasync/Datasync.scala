79405cec68b33c3e42d668cd5f9a53209d39e6e8
Dmitry Melnichenko <sloth.spot@gmail.com>
2010-11-07 01:27:08 +0200

    Add command-line options processing

--- src/name/dmitrym/datasync/Datasync.scala
+++ src/name/dmitrym/datasync/Datasync.scala
@@ -1,22 +1,84 @@
+/**
+ * @author Dmitry Melnichenko
+ */
 package name.dmitrym.datasync
 
-import java.io.File
-
+/**
+ * DataSync main object, application entry point located here
+ */
 object Datasync {
+    /**
+     * used to initialize sync with default cfg
+     */
     def syncWithDefaultCfg {
-      println("Using default cfg: sync.cfg")
+      println( "Using default cfg: sync.cfg" )
     }
 
+    /**
+     * used to initialize sync with specified config
+     * @param cfgname specified config file name
+     */
+    def syncWithNamedCfg( cfgname : String ) {
+        println( "Using cfg from params: " + cfgname )
+    }
+
+    /**
+     * starts sync process with specified config
+     * @param cfgname specified full path to config file
+     */
+    def syncWithCfg( cfgname : String ) {
+      println( "Using cfg: " + cfgname )
+    }
+
+    /**
+     * process help command-line option
+     */
+    def help {
+        println( "DataSync - data synchronizer with configs written in SyncSpec" )
+        println( "\t-help\t\tprints this help message" )
+        println( "\t-version\tprints program version" )
+        println( "\t-cfg [filename]\tspecifies config file to use" )
+    }
+
+    /**
+     * process version command-line option
+     */
+    def version {
+        println( "DataSync version 0.0.1 -- Copyright 2010, Dmitry Melnichenko" )
+    }
+
+    /**
+     * process command line arguments
+     * @param args arguments can be: <i>-help</i>, <i>-version</i>, <i>-cfs [filename]</i>
+     */
+    def processCommandLine( args : Array[String] ) {
+      if ( args.contains( "-help" ) ) {
+          help
+      } else if ( args.contains( "-version" ) ) {
+          version
+      } else {
+          val idx = args.indexOf( "-cfg" )
+          if ( idx > -1 ) {
+            try {
+              syncWithNamedCfg( args( idx + 1 ) )
+            } catch {
+                case ex : Exception =>
+                  println( "Caugt exception: " + ex.getMessage )
+                  ex.printStackTrace
+                  syncWithDefaultCfg
+            }
+          } else {
+              syncWithDefaultCfg
+          }
+      }
+    }
+
+    /**
+     * Application entry point
+     * @param args command-line arguments
+     */
     def main( args : Array[String] ) {
-      println("Hello from DataSync")
-      args.length match {
-          case 0 =>
-            syncWithDefaultCfg
-          case _ if (new File(args(0))).exists =>
-            println("Using custom cfg: " + args(0))
-          case _ =>
-            syncWithDefaultCfg
-        }
+      processCommandLine( args )
     }
 }
 
