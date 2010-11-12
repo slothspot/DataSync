23f6bdc6f402750d0a1c82e664e51be28195be17
Dmitry Melnichenko <dmitry.m@samsung.com>
2010-11-12 18:40:04 +0200

    Add stub for command processing in Parser2

--- src/name/dmitrym/syncspec/Parser2.scala
+++ src/name/dmitrym/syncspec/Parser2.scala
@@ -1,12 +1,42 @@
 package name.dmitrym.syncspec
 
+import scala.Enumeration
 import scala.util.parsing.combinator.syntactical.StandardTokenParsers
 
 class Parser2 extends StandardTokenParsers {
+	object Command extends Enumeration {
+		type Command = Value
+		val Copy, Sync, Move = Value
+	}
+	object Location extends Enumeration {
+		type Location = Value
+		val Source, Destination = Value
+	}
+
 	lexical.reserved += ("copy", "move", "sync", "from", "to")
-	def operation = command ~ source ~ destination | command ~ destination ~ source
-	def command = "copy" | "move" | "sync"
-	def source = "from" ~ location
-	def destination = "to" ~ location
+	def operation = (command ~ source ~ destination | command ~ destination ~ source) ^^ {
+		case Command.Copy ~ (Location.Source, src) ~ (Location.Destination, dst) => println("Operation: copy")
+		case Command.Move ~ _ ~ _ => println("Operation: move")
+		case Command.Sync ~ _ ~ _ => println("Operation: sync")
+	}
+	def command : Parser[Command.Command] = ("copy" | "move" | "sync") ^^ {
+		case "copy" => println("command_copy"); Command.Copy
+		case "move" => println("command_move"); Command.Move
+		case "sync" => println("command_sync"); Command.Sync
+	}
+	def source : Parser[(Location.Location, String)] = ("from" ~ location) ^^ {
+		case "from" ~ loc => println("Src loc: " + loc); (Location.Source, loc)
+	}
+	def destination : Parser[(Location.Location, String)] = ("to" ~ location) ^^ {
+		case "to" ~ loc => println("Dest loc: " + loc); (Location.Destination, loc)
+	}
 	def location = stringLit
+
+	def doMatchOperation( op : String ) = {
+		operation( new lexical.Scanner( op ) ) match {
+			case Success(s, _) => println("doMatchOperation: Success: " + s)
+			case Failure(s, _) => println("doMatchOperation: Failure: " + s)
+			case Error(s, _) => println("doMatchOperation: Error: " + s)
+		}
+	}
 }
