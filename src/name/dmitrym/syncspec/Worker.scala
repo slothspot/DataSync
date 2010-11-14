db537b2dfbebcbeb2e5168c843ca01a738f33114
Dmitry Melnichenko <sloth.spot@gmail.com>
2010-11-14 10:12:56 +0200

    Addutility functions to SyncSpec Worker (dir listing generation, sorting)

--- src/name/dmitrym/syncspec/Worker.scala
+++ src/name/dmitrym/syncspec/Worker.scala
@@ -1,10 +1,35 @@
 package name.dmitrym.syncspec
 
+import java.io.File
+import scala.collection.mutable.ArrayBuilder
+
 /**
  * Worker executes SyncSpec operations
  * @author Dmitry Melnichenko
  */
 object Worker {
+    class SyncEntry(name : String, size : Long, lastModified : Long) {
+        def getName = name
+        def getSize = size
+        def getLastModified = lastModified
+    }
+
+    def listDir(dir : File) : Array[SyncEntry] = {
+        var filesList = new ArrayBuilder.ofRef[SyncEntry]
+        val lst = dir.listFiles
+        lst.foreach( f =>
+            if(! f.isDirectory) filesList += file2SyncEntry(f)
+            else listDir(f).foreach( f => filesList += f )
+        )
+        filesList.result.sortWith( sortByNameAsc )
+    }
+
+    def file2SyncEntry( f : File ) = new SyncEntry(f.getCanonicalPath, f.length, f.lastModified)
+    def sortByNameAsc( a : SyncEntry, b : SyncEntry ) = a.getName.compareToIgnoreCase(b.getName) < 0
+    def sortByNameDsc( a : SyncEntry, b : SyncEntry ) = a.getName.compareToIgnoreCase(b.getName) > 0
+    def sortBySizeAsc( a : SyncEntry, b : SyncEntry ) = a.getSize < b.getSize
+    def sortBySizeDsc( a : SyncEntry, b : SyncEntry ) = a.getSize > b.getSize
+
     /**
      * Copy content of <i>from</i> path to <i>to</i> path
      * @param from path to source folder
@@ -12,6 +37,15 @@ object Worker {
      */
     def copy(from : String, to : String) : Boolean = {
         println("Simple copy of \"" + from + "\" to \"" + to + "\"")
+        val srcF = new File(from)
+        if(! srcF.exists ) false
+        val dstF = new File(to)
+        if(! dstF.exists && ! dstF.mkdirs ) false
+        val srcLst = listDir(srcF)
+        val dstLst = listDir(srcF)
+        //    1, 2, 3, 4, 5
+        //    1,    3, 4,
+        // =>    2,       5
         false
     }
 
