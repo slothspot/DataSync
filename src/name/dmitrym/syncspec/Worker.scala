04641a70850d9b527ec3fadce3428206e26da294
Dmitry Melnichenko <dmitry.m@samsung.com>
2010-11-15 18:15:20 +0200

    Add SyncEntry constructor(File,File)

--- src/name/dmitrym/syncspec/Worker.scala
+++ src/name/dmitrym/syncspec/Worker.scala
@@ -8,27 +8,37 @@ import scala.collection.mutable.ArrayBuilder
  * @author Dmitry Melnichenko
  */
 object Worker {
-    class SyncEntry(name : String, size : Long, lastModified : Long) {
+    class SyncEntry(parent : String, name : String, size : Long, lastModified : Long) {
+        def this( parent : File, f : File ) = this(parent.getCanonicalPath, f.getCanonicalPath.substring(parent.getCanonicalPath.length+1), f.length, f.lastModified)
+        def getParent = parent
         def getName = name
         def getSize = size
         def getLastModified = lastModified
     }
 
-    def listDir(dir : File) : Array[SyncEntry] = {
+    def listDir( dir : File ) : Array[SyncEntry] = listDir( dir, dir )
+
+    def listDir(parent : File, dir : File) : Array[SyncEntry] = {
         var filesList = new ArrayBuilder.ofRef[SyncEntry]
         val lst = dir.listFiles
         lst.foreach( f =>
-            if(! f.isDirectory) filesList += file2SyncEntry(f)
-            else listDir(f).foreach( f => filesList += f )
+            if(! f.isDirectory) filesList += new SyncEntry(parent, f)
+            else listDir(parent, f).foreach( f => filesList += f )
         )
         filesList.result.sortWith( sortByNameAsc )
     }
 
-    def file2SyncEntry( f : File ) = new SyncEntry(f.getCanonicalPath, f.length, f.lastModified)
+    @deprecated("Deprecated by SyncEntry overloaded constructor")
+    def file2SyncEntry( parent : File, f : File ) = new SyncEntry(parent.getCanonicalPath, f.getCanonicalPath.substring(parent.getCanonicalPath.length+1) , f.length, f.lastModified)
     def sortByNameAsc( a : SyncEntry, b : SyncEntry ) = a.getName.compareToIgnoreCase(b.getName) < 0
     def sortByNameDsc( a : SyncEntry, b : SyncEntry ) = a.getName.compareToIgnoreCase(b.getName) > 0
     def sortBySizeAsc( a : SyncEntry, b : SyncEntry ) = a.getSize < b.getSize
     def sortBySizeDsc( a : SyncEntry, b : SyncEntry ) = a.getSize > b.getSize
+
+    def mergeForCopy( srcLst : Array[SyncEntry], dstLst : Array[SyncEntry] ) : Array[SyncEntry] = {
+      val resLst = new ArrayBuilder.ofRef[SyncEntry]
+      resLst.result
+    }
 
     /**
      * Copy content of <i>from</i> path to <i>to</i> path
@@ -42,7 +52,8 @@ object Worker {
         val dstF = new File(to)
         if(! dstF.exists && ! dstF.mkdirs ) false
         val srcLst = listDir(srcF)
-        val dstLst = listDir(srcF)
+        val dstLst = listDir(dstF)
+        val resLst = mergeForCopy(srcLst, dstLst)
         //    1, 2, 3, 4, 5
         //    1,    3, 4,
         // =>    2,       5
