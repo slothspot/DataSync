63dd9d04717bc6c9808d3386510d72c0c0b9dddc
Dmitry Melnichenko <sloth.spot@gmail.com>
2010-12-12 02:25:34 +0200

    Hide Worker internal methods from public access

--- src/name/dmitrym/syncspec/Worker.scala
+++ src/name/dmitrym/syncspec/Worker.scala
@@ -9,7 +9,7 @@ import scala.collection.mutable.ArrayBuilder
  */
 object Worker {
 
-  class SyncEntry(parent: String, name: String, size: Long, lastModified: Long) {
+  private class SyncEntry(parent: String, name: String, size: Long, lastModified: Long) {
     def this(parent: File, f: File) = this (parent.getCanonicalPath, f.getCanonicalPath.substring(parent.getCanonicalPath.length + 1), f.length, f.lastModified)
 
     def getParent = parent
@@ -43,14 +43,14 @@ object Worker {
    * Return directory listing as Array[SyncEntry]
    * @param dir directory to generate listing from
    */
-  def listDir(dir: File): Array[SyncEntry] = listDir(dir, dir)
+  private def listDir(dir: File): Array[SyncEntry] = listDir(dir, dir)
 
   /**
    * Return directory listing as Array[SyncEntry]
    * @param parent parent to pass to SyncEntry ctor
    * @param dir directory to generate listing from
    */
-  def listDir(parent: File, dir: File): Array[SyncEntry] = {
+  private def listDir(parent: File, dir: File): Array[SyncEntry] = {
     var filesList = new ArrayBuilder.ofRef[SyncEntry]
     val lst = dir.listFiles
     lst.foreach(f =>
@@ -61,17 +61,17 @@ object Worker {
   }
 
   @deprecated("Deprecated by SyncEntry overloaded constructor")
-  def file2SyncEntry(parent: File, f: File) = new SyncEntry(parent.getCanonicalPath, f.getCanonicalPath.substring(parent.getCanonicalPath.length + 1), f.length, f.lastModified)
+  private def file2SyncEntry(parent: File, f: File) = new SyncEntry(parent.getCanonicalPath, f.getCanonicalPath.substring(parent.getCanonicalPath.length + 1), f.length, f.lastModified)
 
-  def sortByNameAsc(a: SyncEntry, b: SyncEntry) = a.getName.compareToIgnoreCase(b.getName) < 0
+  private def sortByNameAsc(a: SyncEntry, b: SyncEntry) = a.getName.compareToIgnoreCase(b.getName) < 0
 
-  def sortByNameDsc(a: SyncEntry, b: SyncEntry) = a.getName.compareToIgnoreCase(b.getName) > 0
+  private def sortByNameDsc(a: SyncEntry, b: SyncEntry) = a.getName.compareToIgnoreCase(b.getName) > 0
 
-  def sortBySizeAsc(a: SyncEntry, b: SyncEntry) = a.getSize < b.getSize
+  private def sortBySizeAsc(a: SyncEntry, b: SyncEntry) = a.getSize < b.getSize
 
-  def sortBySizeDsc(a: SyncEntry, b: SyncEntry) = a.getSize > b.getSize
+  private def sortBySizeDsc(a: SyncEntry, b: SyncEntry) = a.getSize > b.getSize
 
-  def mergeForCopy(srcLst: Array[SyncEntry], dstLst: Array[SyncEntry]): Array[SyncEntry] = {
+  private def mergeForCopy(srcLst: Array[SyncEntry], dstLst: Array[SyncEntry]): Array[SyncEntry] = {
     srcLst.filter(e => !dstLst.contains(e))
   }
 
@@ -91,7 +91,7 @@ object Worker {
    * @param to path to destination folder
    * @param deleteSource delete the source file or no
    */
-  def copy(from: String, to: String, deleteSource: Boolean): Boolean = {
+  private def copy(from: String, to: String, deleteSource: Boolean): Boolean = {
     val srcF = new File(from)
     if (!srcF.exists) {
       false
@@ -180,7 +180,7 @@ object Worker {
    * @param deleteSource delete the source file or no
    * @param p path to file with rename rules
    */
-  def copy(from: String, to: String, p: String, deleteSource: Boolean): Boolean = {
+  private def copy(from: String, to: String, p: String, deleteSource: Boolean): Boolean = {
     false
   }
 
